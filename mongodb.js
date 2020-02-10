899261e3f86320a1e4e977a02c80b164f0a2259d
Huy Truong <huy.truong@mevry.com>
2020-02-10 20:36:52 +0700

    create document by ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,22 +1,27 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.id.length);
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=> {
     if(error)
         return console.log('Unable to connect to database!');
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Huy',
-    //     age: 21
-    // }, {forceServerObjectId: true}, (error, result)=>{
-    //     if(error) return console.log('Unable to insert user');
+    db.collection('users').insertOne({
+        _id: id,
+        name: 'Huy',
+        age: 21
+    }, {forceServerObjectId: true}, (error, result)=>{
+        if(error) return console.log('Unable to insert user');
 
-    //     console.log(result.ops)
-    // });
+        console.log(result.ops)
+    });
 
     // db.collection('users').insertMany([
     //     {name: 'Tin', age: 22},
@@ -27,13 +32,13 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=> {
     //     console.log(result.ops);
     // }) 
 
-    db.collection('tasks').insertMany([
-        {description: 'nodejs', completed: false},
-        {description: 'reactjs', completed: true},
-        {description: 'C#', completed: false}
-    ], (error, record) => {
-        if(error) return console.log(error);
+    // db.collection('tasks').insertMany([
+    //     {description: 'nodejs', completed: false},
+    //     {description: 'reactjs', completed: true},
+    //     {description: 'C#', completed: false}
+    // ], (error, record) => {
+    //     if(error) return console.log(error);
 
-        console.log(record.ops);
-    })
+    //     console.log(record.ops);
+    // })
 });

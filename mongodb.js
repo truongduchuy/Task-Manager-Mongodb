f1fa2233efffc2c313c405624503c4aec4cc58df
Huy Truong <huy.truong@mevry.com>
2020-02-10 22:04:44 +0700

    read data with find and findOne

--- mongodb.js
+++ mongodb.js
@@ -3,42 +3,22 @@ const {MongoClient, ObjectID} = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID();
-console.log(id);
-console.log(id.id.length);
-console.log(id.getTimestamp());
-
 MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client)=> {
     if(error)
         return console.log('Unable to connect to database!');
     const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        _id: id,
-        name: 'Huy',
-        age: 21
-    }, {forceServerObjectId: true}, (error, result)=>{
-        if(error) return console.log('Unable to insert user');
+    // db.collection('users').findOne({name: 'thanh'}, (error, user)=>{
+    //     if(error) return console.log('Unable to find user!');
 
-        console.log(result.ops)
+    //     console.log(user)
+    // });
+
+    db.collection('tasks').findOne({_id: new ObjectID("5e414fb6615ef6027804aab9")}, (error, task) =>{
+        console.log(task);
     });
 
-    // db.collection('users').insertMany([
-    //     {name: 'Tin', age: 22},
-    //     {name: "thanh", age: 22}
-    // ], (error, result) => {
-    //     if(error) return console.log('Unable to insert documents!');
-
-    //     console.log(result.ops);
-    // }) 
-
-    // db.collection('tasks').insertMany([
-    //     {description: 'nodejs', completed: false},
-    //     {description: 'reactjs', completed: true},
-    //     {description: 'C#', completed: false}
-    // ], (error, record) => {
-    //     if(error) return console.log(error);
-
-    //     console.log(record.ops);
-    // })
+    db.collection('tasks').find({completed: false}).toArray((error, tasks)=>{
+        console.log(tasks);
+    });
 });

354a31c0a88c780ed4a310a0b322d4b97d9bbe19
Huy Truong <huy.truong@mevry.com>
2020-02-11 16:20:10 +0700

    validator model 1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -7,14 +7,21 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String, 
+        required: true
     },
     age: {
-        type: Number
+        type: Number,
+        validate(value) {
+            if(value < 0)
+                throw new Error('Age must be a positive number');
+        }
     }
 });
 
-const huy = new User({name: 'Huy', age: 22});
+// const huy = new User({ });  // error: name is required
+// const huy = new User({ name: huy }); 
+const huy = new User({ name: 'huy', age: -1 }); // error: ge must be a positive number
 
 huy.save().then(() =>{
     console.log(huy);
@@ -22,22 +29,22 @@ huy.save().then(() =>{
     console.log('Error: '+error);
 });
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    }
-});
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// });
 
-const task = new Task({
-    description: 'Sql',
-    completed: true
-});
+// const task = new Task({
+//     description: 'Sql',
+//     completed: true
+// });
 
-task.save().then(()=>{
-    console.log(task);
-}).catch(error => {
-    console.log(error);
-});
+// task.save().then(()=>{
+//     console.log(task);
+// }).catch(error => {
+//     console.log(error);
+// });

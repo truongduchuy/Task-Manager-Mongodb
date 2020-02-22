d7463ae8b2b88409847ed1ddb78043a8e80760d6
Huy Truong <huy.truong@mevry.com>
2020-02-22 11:08:49 +0700

    get and getById

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,74 +1,25 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
-    name: {
-        type: String, 
-        trim: true,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value))
-                throw new Error('Email is invalid!');
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if(value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain "password"');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if(value < 0)
-                throw new Error('Age must be a positive number');
-        }
-    }
-});
+// const huy = new User({ name: '  HUY  ', email: ' HUY0935903718@gmail.com   ', password: 'dfdd' }); // error: age must be a positive number
 
-const huy = new User({ name: '  HUY  ', email: ' HUY0935903718@gmail.com   ', password: 'dfdd' }); // error: ge must be a positive number
+// huy.save().then(() =>{
+//     console.log(huy);
+// }).catch(error =>{
+//     console.log('Error: '+error);
+// });
 
-huy.save().then(() =>{
-    console.log(huy);
-}).catch(error =>{
-    console.log('Error: '+error);
-});
 
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
+// const task = new Task({
+//     description: 'Sql',
+// });
 
-const task = new Task({
-    description: 'Sql',
-});
-
-task.save().then(()=>{
-    console.log(task);
-}).catch(error => {
-    console.log(error);
-});
+// task.save().then(()=>{
+//     console.log(task);
+// }).catch(error => {
+//     console.log(error);
+// });

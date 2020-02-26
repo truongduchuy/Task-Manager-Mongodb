22748b2a3751a7935450429b9101c39de4f0864a
Huy Truong <huy.truong@mevry.com>
2020-02-26 15:58:04 +0700

    middleware authen

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// app.use((req, res, next) => {
+//     res.status(503).send({message: 'the server under maintenance, please check back soon'});
+// })
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 router.post('/users/signup', async (req, res) => {
     const user = new User(req.body);
@@ -36,13 +37,8 @@ router.post('/users', async (req, res) => {
     }
 });
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.send(users);
-    } catch (e) {
-        res.sendStatus(500).send();
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 });
 
 router.get('/users/:id', async (req, res) => {

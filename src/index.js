0dd3775b903fae71a717dbb73b392ce1aeaa3e02
Huy Truong <huy.truong@mevry.com>
2020-02-26 09:01:30 +0700

    bcryptjs npm

--- src/index.js
+++ src/index.js
@@ -13,3 +13,39 @@ app.use(taskRouter);
 app.listen(port, () => {
     console.log(`Server is up on port  ${port}`);
 });
+
+const bcrypt = require('bcryptjs');
+
+const myFunction = async () => {
+    const password = 'Huy123';
+    
+    
+    bcrypt.hash(password, 8).then(hashedPassword => {
+        bcrypt.compare(password, hashedPassword).then(isMatch => {
+            console.log(isMatch)
+        }).catch(e => {
+            console.log(e)
+        })
+    }).catch(e => {
+        console.log(e)
+    })
+    
+    // promise chaining
+    bcrypt.hash(password, 8).then(hashedPassword => {
+        console.log(password, hashedPassword)
+        return bcrypt.compare(password, hashedPassword);
+    }).then(isMatch => {
+        console.log(isMatch);
+    }).catch(e => {
+        console.log(e)
+    })
+
+    // async await
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password, hashedPassword)
+    const isMatch = await bcrypt.compare('Huy123', hashedPassword);
+    console.log(isMatch)
+}
+
+myFunction()

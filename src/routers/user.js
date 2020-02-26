0dfbd4c8e55953d47bf1ff7df54478816ec1dec5
Huy Truong <huy.truong@mevry.com>
2020-02-26 16:26:25 +0700

    logout

--- src/routers/user.js
+++ src/routers/user.js
@@ -26,6 +26,30 @@ router.post('/users/login', async (req, res) => {
     }
 })
 
+router.post('/users/logout', auth, async (req,res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(item => item.token !== req.token);
+   
+        await req.user.save();
+
+        res.send();
+    } catch (e) {
+        res.status(500).send()
+    }
+});
+
+router.post('/users/logoutAll', auth, async (req,res) => {
+    try {
+        req.user.tokens = [];
+   
+        await req.user.save();
+
+        res.send();
+    } catch (e) {
+        res.status(500).send()
+    }
+});
+
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
 

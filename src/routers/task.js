b49ec6d733966a8818d89f18702913b0b5dd4f35
Huy Truong <huy.truong@mevry.com>
2020-03-01 09:12:18 +0700

    filter, limit, skip, sorting in populate

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,10 +3,34 @@ const router = new express.Router()
 const Task = require('../models/task');
 const auth = require('../middleware/auth');
 
+//GET /tasks?completed=true
+//GET /tasks?limit=2&skip=1
+//GET /tasks?sortBy=createdAt:desc
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+    const sort = {}
+
+    if(req.query.completed) {
+        match.completed = req.query.completed === 'true';
+    }
+
+    if(req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+    }
+
     try {
         // const tasks = await Task.find({owner: req.user._id});
-        await req.user.populate('tasks').execPopulate();
+        await req.user.populate({
+            path: 'tasks',
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate();
+
         res.send(req.user.tasks);
      } catch (e) {
         res.sendStatus(500).send();
